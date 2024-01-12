f9875f3f0f6322f823547e9468fa45480c870a21
jacoder222 <bjuop222@gmail.com>
2024-01-12 17:24:05 +0900

    add: Add More Movies

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/img"));
+app.use(express.static(__dirname + "/feature_ranking"));
+
 app.set("view engine", "ejs");
 
 const { MongoClient, Timestamp } = require("mongodb");
@@ -22,15 +25,34 @@ new MongoClient(url)
     console.log(err);
   });
 
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YTIwODRmMTRjN2Q4YmVkYTUwN2Y2Y2JhOTAzY2JjMCIsInN1YiI6IjY1OTdhMzIyZDdhNzBhMTIyZTZhNWJlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ac_Stz4Gal2NG_KroSZ8NaNIQ-Y8pO-t-kF2A03CjLs",
+  },
+};
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/main.html");
 });
 
 app.get("/search", (req, res) => {
-  const imagePath = "/js.png";
-  res.render("search.ejs", { imagePath: imagePath });
+  res.render("search.ejs");
 });
 
 app.get("/ranking", (req, res) => {
-  res.render("ranking.ejs");
+  let currentPage = 0;
+  const url = `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${
+    currentPage + 1
+  }`;
+  fetch(url, options)
+    .then((response) => response.json())
+    .then(({ results }) => {
+      res.render("ranking.ejs", { results: results });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
